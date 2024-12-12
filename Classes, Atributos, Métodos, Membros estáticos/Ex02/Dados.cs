3f79ab4e433037e74f04d31139fef7d8b751d2d8
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-12-12 17:47:02 -0300

    Exercício 02
    Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário
    médio dos funcionários.

---
+++ Classes, Atributos, Métodos, Membros estáticos/Ex02/Dados.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    internal class Dados
+    {
+        //Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário
+        //médio dos funcionários.
+
+        public string N;
+        public double S;
+
+    }
+}
---
+++ Classes, Atributos, Métodos, Membros estáticos/Ex02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+namespace Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário
+            //médio dos funcionários.
+
+            Dados Nome, Nome2, Salario, Salario2;
+            double media;
+
+            Nome = new Dados();
+            Salario = new Dados();
+            Nome2 = new Dados();
+            Salario2 = new Dados();
+
+            Console.Write("Informe o nome: ");
+            Nome.N = Console.ReadLine();
+
+            Console.Write("Informe o salario: ");
+            Salario.S = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Informe os dados do segundo funcionario...");
+            Console.WriteLine();
+
+            Console.Write("Informe o nome: ");
+            Nome2.N = Console.ReadLine();
+
+            Console.Write("Informe o Salario: ");
+            Salario2.S = double.Parse(Console.ReadLine());
+
+            media = (Salario.S + Salario2.S) / 2;
+
+            Console.WriteLine("Salário médio = " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
